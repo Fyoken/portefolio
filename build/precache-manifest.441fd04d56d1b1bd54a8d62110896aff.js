21f2e0eeb70af7b53a4c4bd99bf69145d68d4e4f
Fyoken <mathias.barbosa.67@gmail.com>
2024-11-13 17:29:13 +0100

    change homepage

---
+++ build/precache-manifest.441fd04d56d1b1bd54a8d62110896aff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad7c5db9fb2348b315297b1feb8764e",
+    "url": "/Porte_folio/index.html"
+  },
+  {
+    "revision": "f65b439a68202b7a32fe",
+    "url": "/Porte_folio/static/css/main.14d8a01a.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/Porte_folio/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "f65b439a68202b7a32fe",
+    "url": "/Porte_folio/static/js/main.a181cbf3.chunk.js"
+  },
+  {
+    "revision": "d5da436ac8298803b9dd",
+    "url": "/Porte_folio/static/js/runtime~main.d7d1306e.js"
+  }
+]);
