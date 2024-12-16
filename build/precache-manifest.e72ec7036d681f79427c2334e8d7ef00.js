5712ef39691b446851d924f4e88d662281e4ba52
Fyoken <mathias.barbosa.67@gmail.com>
2024-12-16 14:12:36 +0100

    Updating CV

---
+++ build/precache-manifest.e72ec7036d681f79427c2334e8d7ef00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629e278ea5410d10fbbf4cbc9a328368",
+    "url": "/portefolio/index.html"
+  },
+  {
+    "revision": "fe32cecad88e87f858da",
+    "url": "/portefolio/static/css/main.803ff84f.chunk.css"
+  },
+  {
+    "revision": "cb94078f0ed4cf8bf980",
+    "url": "/portefolio/static/js/2.990dd9f0.chunk.js"
+  },
+  {
+    "revision": "fe32cecad88e87f858da",
+    "url": "/portefolio/static/js/main.e2ebe71d.chunk.js"
+  },
+  {
+    "revision": "478c0f91fb2f52d36209",
+    "url": "/portefolio/static/js/runtime~main.32a7b7e9.js"
+  }
+]);
