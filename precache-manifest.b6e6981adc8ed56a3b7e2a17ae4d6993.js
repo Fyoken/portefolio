83479e185ca6cd8048477c6c769bb8d739614782
Fyoken <mathias.barbosa.67@gmail.com>
2024-12-18 12:56:35 +0100

    Updates

--- precache-manifest.b6e6981adc8ed56a3b7e2a17ae4d6993.js
+++ precache-manifest.b6e6981adc8ed56a3b7e2a17ae4d6993.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82035aa3b5bda2c98346fee0108f9c0a",
+    "revision": "525b8e04e135b709b74b0d563ee8a09f",
     "url": "/portefolio/index.html"
   },
   {
-    "revision": "cf852a146d86c028f52c",
+    "revision": "3b9ab31c846e8d41f81c",
     "url": "/portefolio/static/css/main.803ff84f.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portefolio/static/js/2.990dd9f0.chunk.js"
   },
   {
-    "revision": "cf852a146d86c028f52c",
-    "url": "/portefolio/static/js/main.313b7d76.chunk.js"
+    "revision": "3b9ab31c846e8d41f81c",
+    "url": "/portefolio/static/js/main.69df01c7.chunk.js"
   },
   {
     "revision": "478c0f91fb2f52d36209",
